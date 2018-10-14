cd1ccb159eb0e57f1e04554bbfa8566c9b3033a3
Johann Schatzer <j.schatzer@tin.it>
2018-10-14 15:33:54 +0200

    update

--- html.lisp
+++ html.lisp
@@ -90,7 +90,7 @@
 
 ;;;;;
 ;(route-html /pkgdoc19 ()    ;from html-pkg-doc
-(route-html /pkgdoc20 ()   ;    is the same as  ;(route-html /pkgdoc19 ()    ;from html-pkg-doc
+(route-html /pkgdoc0 ()   ;    is the same as  ;(route-html /pkgdoc19 ()    ;from html-pkg-doc
   ((i-css jts)
    (i-css "/jquery.splitter/css/jquery.splitter.css")
    (css 
@@ -116,6 +116,39 @@
                                                                 ((equal (y.item.text) "quicklisp") (lisp (sys-tree2ul (html-create-menu (cons "MENU" (pkg-doc::quicklisp-systems))) "menu")))
                                                                 ((equal (y.item.text) "local-libs") (lisp (sys-tree2ul (html-create-menu (cons "MENU" (pkg-doc::local-systems))) "menu"))))))
                                           ($ "#menu" (menu (o select (f (a b) ($ "#pkg" (text (b.item.text))))))))))))
+  (js ($ "#content" (height (- window.inner-Height 100)) (split (o orientation "vertical" position "50%" limit 10)))))
+
+
+;geht nicht
+(route-html /pkgdoc1 ()   ;    is the same as  ;(route-html /pkgdoc19 ()    ;from html-pkg-doc
+  ((i-css jts)
+   (i-css "/jquery.splitter/css/jquery.splitter.css")
+   (css 
+     ("#navigation.ui-menu .ui-menu-item" float left)
+     ;     ("#content #menu" width 200px)
+     ("#menu" width 200px)
+     ;     ("#menu" columns 4)    
+     ("#content ul" columns 2)     ; geht prinzipiell
+     )
+   (css (".splitter_panel .vsplitter" background-color gray width 3px)))
+  (list2ul '("packages" "quicklisp" "local-libs" "cl-apropos" "ql-apropos" "help" "features" "modules") "navigation")
+  (:br)
+  (:div :id "content"
+        (:iframe :src "/src/pkgtree" :name "tree" :id "tree" :marginwidth "0" :marginheight "0" :scrolling "yes" "") ;there must be content, to have a closing tag
+        (:iframe :src "/src/info" :name "info" :id "info" :marginwidth "0" :marginheight "0" :scrolling "yes" ""))
+  (:div :id "pkg")
+  (i-js jq3.3)
+  (i-js ui1.12)
+  (i-js "/jquery.splitter/js/jquery.splitter.js")
+  (js ($ "#navigation" (menu (o select (f (x y) 
+                                          ($ "#content" (html 
+                                                          (cond ((equal (y.item.text) "packages") (lisp (sys-tree2ul (html-create-menu (cons "MENU" (pkg-doc::current-packages))) "menu")))
+                                                                ((equal (y.item.text) "quicklisp") (lisp (sys-tree2ul (html-create-menu (cons "MENU" (pkg-doc::quicklisp-systems))) "menu")))
+                                                                ((equal (y.item.text) "local-libs") (lisp (sys-tree2ul (html-create-menu (cons "MENU" (pkg-doc::local-systems))) "menu"))))))
+ ;                                         ($ "#menu" (menu (o select (f (a b) ($ "#pkg" (text (b.item.text)))))))
+                                           ($ "#menu" (menu (o select (f (a b) (load (format nil "/src/pkgtree?~a" (b.item.text)))))))
+                                         
+                                          )))))
   (js ($ "#content" (height (- window.inner-Height 100)) (split (o orientation "vertical" position "50%" limit 10)))))
 
 
@@ -179,8 +212,161 @@ $('ul').on('click', 'a', function(){
   (js ($ "ul" (on("click" "a" (f0 ($ ("#hello" top.frames["info"].document)
            (load "/info"))))))))
 
-;MENU https://jqueryui.com/menu/#default
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,
+;use jqui selcteble statt jq on  <---??
+(route-html /src/pkgtree ()
+  ((i-css "/jquery-simplefolders/main.css")
+   (css (li white-space nowrap) 
+        ("a:hover" background-color "#81F7BE")))
+;  (:h1 "Homepage")
+  (tree2ul lst)
+  (i-js jqm)
+  (i-js "/jquery-simplefolders/main.js")
+(js ($ "#tree" (on "click" "#tree div" (f0 
+  ($ ("#hello" (c (p top.frames "info") document)) (html "Hakg jlag")))))))
+
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,
+ ; das geht
+ (route-html /src/pkgtree ()
+  ((i-css "/jquery-simplefolders/main.css")
+   (css (li white-space nowrap) 
+        ("a:hover" background-color "#81F7BE")))
+;  (:h1 "Homepage")
+  (tree2ul lst)
+  (i-js jqm)
+  (i-js "/jquery-simplefolders/main.js")
+(js 
+  ($ ("#hello" (c (p top.frames "info") document)) (text "Hakg jlag"))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+ (route-html /src/pkgtree ()
+  ((i-css "/jquery-simplefolders/main.css")
+   (css (li white-space nowrap) 
+        ("a:hover" background-color "#81F7BE")))
+  (tree2ul lst)
+  (i-js jqm)
+  (i-js "/jquery-simplefolders/main.js")
+(js 
+  ($ ("#hello" (c (p top.frames "info") document)) (text (lisp (h:stg (repl-utilities:arglist 'clim:present)))))))
+
+(route-html /src/pkgtree ()
+  ((i-css "/jquery-simplefolders/main.css")
+   (css (li white-space nowrap) 
+        ("a:hover" background-color "#81F7BE")))
+  (tree2ul lst)
+  (i-js jqm)
+  (i-js "/jquery-simplefolders/main.js")
+  (js ($ ".tree" (on "click" "li" (f_  
+ ;                                   (console.log _.target.text-content)
+                                     (console.log ($ this (text)))
+                                   
+                                    )))))
+
+;;;;;;;;;;;;;; geth gut ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(route-html /src/pkgtree ()
+  ((i-css "/jquery-simplefolders/main.css")
+   (css (li white-space nowrap) 
+        ("a:hover" background-color "#81F7BE")))
+  (tree2ul lst)
+  (i-js jqm)
+  (i-js "/jquery-simplefolders/main.js")
+  (js ($ ".tree" (on "click" "a" (f0 (console.log ($ this (text))))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;geht gut
+(route-html /src/pkgtree ()
+  ((i-css "/jquery-simplefolders/main.css")
+   (css (li white-space nowrap) 
+        ("a:hover" background-color "#81F7BE")))
+  (tree2ul lst)
+  (i-js jqm)
+  (i-js "/jquery-simplefolders/main.js")
+  (js ($ ".tree" (on "click" "a" (f0 
+                                   ($ ("#hello" (c (p top.frames "info") document)) 
+                                        (text ($ this (text)))))))))
+
+#|
+;;;;;;;;
+;geht ;;; http://acer/src/pkgtree?pkg=CL-FAD
+;optional geht nicht??
+;(route-html /src/pkgtree (&optional (pkg "CL-FAD"))
+(route-html /src/pkgtree (pkg)
+  ((i-css "/jquery-simplefolders/main.css")
+   (css (li white-space nowrap) 
+        ("a:hover" background-color "#81F7BE")))
+;  (tree2ul lst)
+  (tree2ul (pkg-doc:pkg-tree pkg))
+  (i-js jqm)
+  (i-js "/jquery-simplefolders/main.js")
+  (js ($ ".tree" (on "click" "a" (f0 
+                                   ($ ("#hello" (c (p top.frames "info") document)) 
+                                        (text ($ this (text)))))))))
+
+|#
+
+;(h:p lst (pkg-doc:pkg-tree "CL-FAD"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+#|
+(route-html /src/pkgtree ()
+  ((i-css "/jquery-simplefolders/main.css")
+   (css (li white-space nowrap) 
+        ("a:hover" background-color "#81F7BE")))
+  (tree2ul lst)
+  (i-js jqm)
+  (i-js "/jquery-simplefolders/main.js")
+  (js ($ ".tree" (on "click" "a" (f0 
+                                   ($ ("#hello" (c (p top.frames "info") document)) 
+                                        (text (lisp (h:stg (repl-utilities:arglist (find-symbol (string-upcase (#~s'function:-'' ($ this (text)))))))))))))))
+
+|#
+
+
+                                   
+
+
+;                               ($ ("#hello" (c (p top.frames "info") document)) 
+;                                  _.target.text-content
+;                                  ))))))
+
+;(html "hellllo")
+;          (html ($ this (text))) 
+;           )))))))
+
+;;;;; test
+#;(route-html /src/pkgtree ()
+  ((i-css "/jquery-simplefolders/main.css")
+   (css (li white-space nowrap) 
+        ("a:hover" background-color "#81F7BE")))
+;  (:h1 "Homepage")
+  (tree2ul lst)
+  (i-js jqm)
+  (i-js "/jquery-simplefolders/main.js")
+(js  ($ "#tree" (on "click" "#tree li" (f0
+                    (alert ($ this (text))))))))
+  
+  
+;  ($ "#tree" (on "click" "#tree div" (f0 
+;  ($ ("#hello" (c (p top.frames "info") document)) (html "Hakg jlag")))))))
+ 
+
+;.html($(this).attr('id'));
+
 (route-html /src/info ()
+  ((i-js "https://code.jquery.com/jquery-1.12.4.js")  
+   (i-js "https://code.jquery.com/ui/1.12.1/jquery-ui.js")
+   (i-css "//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css")
+   ;(i-css "/resources/demos/style.css")
+   )
+(:div :id "hello"))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,,
+;MENU https://jqueryui.com/menu/#default
+#;(route-html /src/info ()
   ((i-js "https://code.jquery.com/jquery-1.12.4.js")  
    (i-js "https://code.jquery.com/ui/1.12.1/jquery-ui.js")
    (:script 
@@ -193,6 +379,7 @@ $('ul').on('click', 'a', function(){
    (i-css "/resources/demos/style.css")
    ;(css (".ui-menu" :width 150px))
    )
+(:div :id "hello" "hellllllo")
 #"
 <ul id="menu">
 <li class="ui-state-disabled"><div>Toys (n/a)</div></li>
